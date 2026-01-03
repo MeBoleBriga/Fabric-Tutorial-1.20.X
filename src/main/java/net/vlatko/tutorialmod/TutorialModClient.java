6fdaa132d1438803acadd6c8bb56a6214e8b6fc2
PC <vlatkopaonv09@gmail.com>
2026-01-03 17:16:47 +0100

    added content

---
+++ src/main/java/net/vlatko/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.vlatko.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
