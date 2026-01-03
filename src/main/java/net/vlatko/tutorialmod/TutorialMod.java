6633dff96de44f0729e12aa3477032aa5c7ac17f
PC <vlatkopaonv09@gmail.com>
2026-01-03 21:55:36 +0100

    added raw pink garnet and pink garnet

--- src/main/java/net/vlatko/tutorialmod/TutorialMod.java
+++ src/main/java/net/vlatko/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package net.vlatko.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.vlatko.tutorialmod.Items.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,5 +12,6 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
+		ModItems.registerModItems();
 	}
 }
